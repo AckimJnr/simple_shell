cb66439d175b41c0bed94e5594ce9e326784c5ad
Angela-Nyaka <nyakaangela@gmail.com>
2023-05-25 02:16:16 +0000

    exec_path

--- exec_path.c
+++ exec_path.c
@@ -13,8 +13,9 @@ void exec_path(char *args[], int s_count, char *pg_name)
 	char *path = getenv("PATH");
 	char *token;
 	char *cmd_path = NULL;
-	pid_t pid = fork();
 	int status;
+	pid_t pid;
+	int command_found = 0;
 
 	token = strtok(path, ":");
 	if (path == NULL)
@@ -22,6 +23,30 @@ void exec_path(char *args[], int s_count, char *pg_name)
 		perror(pg_name);
 		exit(EXIT_FAILURE);
 	}
+	while (token != NULL)
+	{
+		size_t path_len = strlen(token) + strlen(args[0]) + 2;
+
+		cmd_path = malloc(path_len);
+		snprintf(cmd_path, path_len, "%s/%s", token, args[0]);
+
+
+		if (access(cmd_path, X_OK) == 0)
+		{
+			command_found = 1;
+			break;
+		}
+		free(cmd_path);
+		cmd_path = NULL;
+		token = strtok(NULL, ":");
+	}
+	if (!command_found)
+	{
+		fprintf(stderr, ER_MSG, pg_name, s_count, args[0]);
+		exit(EXIT_FAILURE);
+	}
+	pid = fork();
+
 	if (pid < 0)
 	{
 		perror(pg_name);
@@ -29,33 +54,15 @@ void exec_path(char *args[], int s_count, char *pg_name)
 	}
 	else if (pid == 0)
 	{
-		while (token != NULL)
+		if (execv(cmd_path, args) < 0)
 		{
-			size_t path_len = strlen(token) + strlen(args[0]) + 2;
-
-			cmd_path = malloc(path_len);
-			snprintf(cmd_path, path_len, "%s/%s", token, args[0]);
-
-
-			if (access(cmd_path, X_OK) == 0)
-			{
-				if (execv(cmd_path, args) < 0)
-				{
-					fprintf(stderr, ER_MSG, pg_name, s_count, args[0]);
-					exit(EXIT_FAILURE);
-				}
-			}
-
-			free(cmd_path);
-			cmd_path = NULL;
-
-			token = strtok(NULL, ":");
+			fprintf(stderr, ER_MSG, pg_name, s_count, args[0]);
+			exit(EXIT_FAILURE);
 		}
-		fprintf(stderr, ER_MSG, pg_name, s_count, args[0]);
-		exit(EXIT_FAILURE);
 	}
 	else
 	{
 		waitpid(pid, &status, 0);
 	}
+	free(cmd_path);
 }
